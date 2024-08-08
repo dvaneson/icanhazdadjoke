3ee4ac901a0ddf35d1e535902695c1267ebfd720
Donovan Ellison <dvaneson@gmail.com>
2024-08-08 13:41:38 -0700

    Cleaning up the main function

--- dad_joke.py
+++ dad_joke.py
@@ -11,6 +11,10 @@ import requests
 DAD_JOKE_BASE_URL = "https://icanhazdadjoke.com"
 HEADERS = {"Accept": "application/json"}
 
+def tell_joke(joke: str) -> None:
+    print(joke)
+    time.sleep(1)
+
 def random_dad_joke() -> str:
     req = requests.get(DAD_JOKE_BASE_URL, headers=HEADERS)
     req_json = req.json()
@@ -21,38 +25,51 @@ def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
     req_json = req.json()
     return req_json["results"]
 
+def random_query_loop(number: int) -> None:
+    i = 0
+    while i < number: 
+        dad_joke = random_dad_joke()
+        tell_joke(dad_joke)
+        i += 1
+    
+    return
+
+def search_query_loop(search: str, number: int, page: int) -> int:
+    dad_jokes = search_dad_jokes(search, page=page, limit=number)
+    page += 1
+
+    length = len(dad_jokes)
+    if length < number:
+        page = 1
+        dad_jokes += search_dad_jokes(search, page=page, limit=number-length)
+
+    for dad_joke in dad_jokes:
+        tell_joke(dad_joke["joke"])
+    
+    return page
+
 def main(search: str, number: int) -> None:
     i = 0
     page = 1
     while i <= 60:
-        j = 0
-        while j < number: 
-            if search:
-                dad_jokes = search_dad_jokes(args.search, page=page, limit=number)
-                page += 1
-
-                length = len(dad_jokes)
-                if length < number:
-                    print("In length < number")
-                    page = 1
-                    dad_jokes += search_dad_jokes(args.search, page=page, limit=number-length)
-
-                for dad_joke in dad_jokes:
-                    print(dad_joke["joke"])
-                j += number
-            else:
-                dad_joke = random_dad_joke()
-                print(dad_joke)
-                j += 1
+        if search:
+            page = search_query_loop(search, number, page)
+        else:
+            random_query_loop(number)
     
         i += 15
-        time.sleep(5)
-        print("")
+
+        # Don't need to wait on the last loop
+        if i < 60:
+            print("--------------------------------------------------------------------------------------")
+            time.sleep(5)
 
     return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    # Also how to handle if number > 40, the API limit? Just error out?
     parser.add_argument("-n", "--number", help="The number of dad jokes to return in each set", type=int, default=1)
     parser.add_argument("-s", "--search", help="A search term to look up", type=str)
     args = parser.parse_args()
