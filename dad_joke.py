2b6af496c43127512096932a92d68c7dd055b456
Donovan Ellison <dvaneson@gmail.com>
2024-08-08 15:51:40 -0700

    Adding some comments for clarity

--- dad_joke.py
+++ dad_joke.py
@@ -9,16 +9,25 @@ import argparse
 import requests
 
 DAD_JOKE_BASE_URL = "https://icanhazdadjoke.com"
-HEADERS = {"Accept": "application/json"}
+HEADERS = {
+    "Accept": "application/json",
+    "User-Agent": "My Library (https://github.com/dvaneson/icanhazdadjoke)"    
+}
 
 def tell_joke(joke: str) -> None:
+    # Print the joke and give a small delay before the next joke is told
     print(joke)
-    time.sleep(1)
+    time.sleep(.1)
 
 def random_dad_joke() -> str:
     req = requests.get(DAD_JOKE_BASE_URL, headers=HEADERS)
-    req_json = req.json()
-    return req_json["joke"]
+    if req.status_code == 200:
+        req_json = req.json()
+        return req_json["joke"]
+    else:
+        # If the request fails for some reason, wait a second and try again
+        time.sleep(1)
+        return random_dad_joke()
 
 def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
     req = requests.get(f"{DAD_JOKE_BASE_URL}/search?page={page}&term={search}&limit={limit}", headers=HEADERS)
@@ -26,13 +35,15 @@ def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
     return req_json["results"]
 
 def random_query_loop(number: int) -> None:
+    # Tell a set of random dad jokes, based on the count
     i = 0
     while i < number: 
-        dad_joke = random_dad_joke()
+        dad_joke = random_dad_joke(number)
         tell_joke(dad_joke)
         i += 1
 
 def search_query_loop(search: str, number: int, page: int) -> int:
+    # Search for dad jokes based on the term, up to the number specified
     dad_jokes = search_dad_jokes(search, page=page, limit=number)
     page += 1
 
@@ -49,21 +60,26 @@ def search_query_loop(search: str, number: int, page: int) -> int:
     return page
 
 def main(search: str, number: int) -> None:
+    # Repeat the loop after the interval (time in seconds), until the total time has been reached
     i = 0
+    interval = 15
+    total = 60
+
+    # For tracking the page number with search results 
     page = 1
-    while i <= 60:
-        # If a search term was specified, use it
+
+    while i <= total:
+        # If a search term was specified, use it, otherwise get n random jokes
         if search:
             page = search_query_loop(search, number, page)
-        # Else, get n random jokes
         else:
             random_query_loop(number)
     
-        i += 15
+        i += interval
         # Don't need to wait on the last loop
-        if i < 60:
+        if i <= total:
             print("--------------------------------------------------------------------------------------")
-            time.sleep(5)
+            time.sleep(interval)
 
     return
 
@@ -74,6 +90,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--search", help="A search term to look up", type=str)
     args = parser.parse_args()
 
+    # Validate the number arg
     if args.number > 0 and args.number <= 40: 
         main(args.search, args.number)
     else:
