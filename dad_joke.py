c3edf5e0669fa0249c5862d0597017dd33bb9ea1
Donovan Ellison <dvaneson@gmail.com>
2024-08-08 14:04:08 -0700

    Adding some comments

--- dad_joke.py
+++ dad_joke.py
@@ -20,6 +20,11 @@ def random_dad_joke() -> str:
     req_json = req.json()
     return req_json["joke"]
 
+def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
+    req = requests.get(f"{DAD_JOKE_BASE_URL}/search?page={page}&term={search}&limit={limit}", headers=HEADERS)
+    req_json = req.json()
+    return req_json["results"]
+
 def random_query_loop(number: int) -> None:
     i = 0
     while i < number: 
@@ -27,19 +32,16 @@ def random_query_loop(number: int) -> None:
         tell_joke(dad_joke)
         i += 1
 
-def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
-    req = requests.get(f"{DAD_JOKE_BASE_URL}/search?page={page}&term={search}&limit={limit}", headers=HEADERS)
-    req_json = req.json()
-    return req_json["results"]
-
 def search_query_loop(search: str, number: int, page: int) -> int:
     dad_jokes = search_dad_jokes(search, page=page, limit=number)
     page += 1
 
+    # If the number of jokes requested was not met, keep querying until the number has been hit
     length = len(dad_jokes)
-    if length < number:
+    while length < number:
         page = 1
         dad_jokes += search_dad_jokes(search, page=page, limit=number-length)
+        length = len(dad_jokes)
 
     for dad_joke in dad_jokes:
         tell_joke(dad_joke["joke"])
@@ -50,13 +52,14 @@ def main(search: str, number: int) -> None:
     i = 0
     page = 1
     while i <= 60:
+        # If a search term was specified, use it
         if search:
             page = search_query_loop(search, number, page)
+        # Else, get n random jokes
         else:
             random_query_loop(number)
     
         i += 15
-
         # Don't need to wait on the last loop
         if i < 60:
             print("--------------------------------------------------------------------------------------")
@@ -65,9 +68,8 @@ def main(search: str, number: int) -> None:
     return
 
 if __name__ == "__main__":
+    # Configuring CLI arguments
     parser = argparse.ArgumentParser()
-
-    # Also how to handle if number > 40, the API limit? Just error out?
     parser.add_argument("-n", "--number", help="The number of dad jokes to return in each set. Default is 1. Must be between 1 and 40.", default=1, type=int)
     parser.add_argument("-s", "--search", help="A search term to look up", type=str)
     args = parser.parse_args()
@@ -75,4 +77,4 @@ if __name__ == "__main__":
     if args.number > 0 and args.number <= 40: 
         main(args.search, args.number)
     else:
-        print(f"You have entered an invalid number {args.number}. Please select a new number between 1 and 40")
+        print(f"ERROR: You have entered an invalid number, {args.number} \nPlease select a new number between 1 and 40")
