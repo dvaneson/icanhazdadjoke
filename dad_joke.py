01e3654ef0c1d79b08eb343fee9afafc6b0780b8
Donovan Ellison <dvaneson@gmail.com>
2024-08-08 13:47:28 -0700

    Adding some error handling for an invalid number

--- dad_joke.py
+++ dad_joke.py
@@ -20,19 +20,17 @@ def random_dad_joke() -> str:
     req_json = req.json()
     return req_json["joke"]
 
-def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
-    req = requests.get(f"{DAD_JOKE_BASE_URL}/search?page={page}&term={search}&limit={limit}", headers=HEADERS)
-    req_json = req.json()
-    return req_json["results"]
-
 def random_query_loop(number: int) -> None:
     i = 0
     while i < number: 
         dad_joke = random_dad_joke()
         tell_joke(dad_joke)
         i += 1
-    
-    return
+
+def search_dad_jokes(search: str, page: int = 1, limit: int = 20) -> list[dict]:
+    req = requests.get(f"{DAD_JOKE_BASE_URL}/search?page={page}&term={search}&limit={limit}", headers=HEADERS)
+    req_json = req.json()
+    return req_json["results"]
 
 def search_query_loop(search: str, number: int, page: int) -> int:
     dad_jokes = search_dad_jokes(search, page=page, limit=number)
@@ -70,8 +68,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Also how to handle if number > 40, the API limit? Just error out?
-    parser.add_argument("-n", "--number", help="The number of dad jokes to return in each set", type=int, default=1)
+    parser.add_argument("-n", "--number", help="The number of dad jokes to return in each set. Default is 1. Must be between 1 and 40.", default=1, type=int)
     parser.add_argument("-s", "--search", help="A search term to look up", type=str)
     args = parser.parse_args()
 
-    main(args.search, args.number)
+    if args.number > 0 and args.number <= 40: 
+        main(args.search, args.number)
+    else:
+        print(f"You have entered an invalid number {args.number}. Please select a new number between 1 and 40")
